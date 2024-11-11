31421726114a37d6b6bbe2c98ff79d934bf4e2cc
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-11-11 03:44:03 +0300

    refactor: implement dispatch

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,19 +3,18 @@ import Post from "./Post/Post";
 import React from "react";
 
 const MyPosts = (props) => {
-    let postsElements = props.posts.map(p => <Post key={p.id}
-                                                   message={p.message}
-                                                   likesCount={p.likesCount}/>)
+    let postsElements = props.posts.map(p =>
+        <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
 
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        props.dispatch({type: 'ADD-POST'});
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
     }
 
     return (
